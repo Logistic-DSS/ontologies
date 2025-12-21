624d63001340ae9a5b2581e95dc3ecf673505c6e
Skrapgon <ISolkinI@yandex.ru>
2025-12-21 17:13:29 +0300

    init commit

---
+++ src/main/java/com/logistic/ontologies/OntologiesApplication.java
@@ -0,0 +1,13 @@
+package com.logistic.ontologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntologiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logistic/ontologies/OntologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistic.ontologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntologiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
